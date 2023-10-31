1497d79a2473a0d6002d7448364bd55c2e2a24d2
matheusc1 <cardoso.matheusbs@gmail.com>
2023-10-30 22:07:27 -0300

    style: add new colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,12 +8,20 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
+      colors: {
+        gray100: '#F2F2F2',
+        gray200: '#D9D9D9',
+        gray300: '#808080',
+        gray400: '#333333',
+        gray500: '#262626',
+        gray600: '#1A1A1A',
+        gray700: '#0D0D0D',
+        purple: '#8284FA',
+        purpleDark: '#5E60CE',
+        blue: '#4EA8DE',
+        blueDark: '#1E6F9F'
+      }
+    }
   },
   plugins: [],
 }
