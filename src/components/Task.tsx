76f88e956d71435a5cea24489a7d47330bd9350c
matheusc1 <cardoso.matheusbs@gmail.com>
2023-11-01 17:56:08 -0300

    feat: create task component

---
+++ src/components/Task.tsx
@@ -0,0 +1,25 @@
+import { Task } from "@/types/task.interface"
+import { Trash2 } from "lucide-react"
+
+interface TaskProps {
+  task: Task
+}
+
+export default function Task({ task }: TaskProps) {
+  return (
+    <div className={`flex w-608 bg-gray500 rounded-lg mb-3 items-center justify-center p-3
+      ${task.completed ? "border-none" : "border border-solid border-gray400"}`}
+    >
+      <input
+        type="checkbox"
+        className="ml-1 w-5 h-5 rounded-full border-2 border-solid
+        bg-gray400 border-blue focus:ring-transparent focus:ring-offset-0 hover:border-blueDark
+        checked:bg-purpleDark checked:hover:bg-purple checked:focus:bg-purpleDark"
+      />
+      <p className={`mx-3 text-sm ${task.completed ? "text-gray300 line-through" : "text-gray100"}`}>
+        {task.desc}
+      </p>
+      <Trash2 className="cursor-pointer text-gray300 hover:text-danger h-7 w-7" />
+    </div>
+  )
+} 
---
+++ src/types/task.interface.ts
@@ -0,0 +1,5 @@
+export interface Task {
+  id: number
+  desc: string
+  completed: boolean
+}
