21be3c9714ed53762c1e6c095051290ef524be41
matheusc1 <cardoso.matheusbs@gmail.com>
2023-11-02 20:40:52 -0300

    refactor: add functions to delete and mark tasks as completed, and fix styles

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -3,23 +3,28 @@ import { Trash2 } from "lucide-react"
 
 interface TaskProps {
   task: Task
+  handleDelete: () => void
+  onChange: () => void
 }
 
-export default function Task({ task }: TaskProps) {
+export default function Task({ task, handleDelete, onChange }: TaskProps) {
   return (
-    <div className={`flex w-736 bg-gray500 rounded-lg mb-3 items-center justify-center p-3
+    <div className={`flex w-736 bg-gray500 rounded-lg mb-3 items-center justify-between p-3
       ${task.completed ? "border-none" : "border border-solid border-gray400"}`}
     >
       <input
+        onChange={onChange}
         type="checkbox"
         className="ml-1 w-5 h-5 rounded-full border-2 border-solid
         bg-gray400 border-blue focus:ring-transparent focus:ring-offset-0 hover:border-blueDark
         checked:bg-purpleDark checked:hover:bg-purple checked:focus:bg-purpleDark"
       />
-      <p className={`mx-3 text-sm ${task.completed ? "text-gray300 line-through" : "text-gray100"}`}>
-        {task.desc}
-      </p>
-      <Trash2 className="cursor-pointer text-gray300 hover:text-danger h-6 w-6" />
+      <div className="flex-grow">
+        <p className={`ml-2 text-sm ${task.completed ? "text-gray300 line-through" : "text-gray100"}`}>
+          {task.desc}
+        </p>
+      </div>
+      <Trash2 onClick={handleDelete} className="cursor-pointer ml-2 text-gray300 hover:text-danger min-w-[24px] h-6" />
     </div>
   )
 }
